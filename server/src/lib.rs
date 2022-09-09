b7114fde949eda16c1c129de87e288cbf6e422f2
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2022-09-09 22:21:52 +0000

    remove unused comments

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -11,27 +11,6 @@ pub mod objects {
 }
 pub mod api {
     pub mod v1alpha1 {
-     tonic::include_proto!("music.v1alpha1");
+        tonic::include_proto!("music.v1alpha1");
     }
 }
-
-/*
-
-pub mod metadata {
-    pub mod v1alpha1 {
-        include!("metadata.v1alpha1.rs");
-    }
-}
-pub mod objects {
-    pub mod v1alpha1 {
-        include!("objects.v1alpha1.rs");
-    }
-}
-pub mod music {
-    pub mod v1alpha1 {
-        include!("music.v1alpha1.rs");
-    }
-}
-
-
-*/

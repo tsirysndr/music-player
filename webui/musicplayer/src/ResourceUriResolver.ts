20780deead4a10c592f9b12bc488458d157148f4
Jacob Stern <jacob.stern@outlook.com>
2022-11-26 19:16:46 -0800

    Fix ResourceUriResolver regex

--- webui/musicplayer/src/ResourceUriResolver.ts
+++ webui/musicplayer/src/ResourceUriResolver.ts
@@ -1,5 +1,5 @@
-let pathSep = '/';
-let coversDir = '';
+let pathSep = "/";
+let coversDir = "";
 
 export const resourceUriResolver = {
   async initializeForNativeWrapper(): Promise<void> {
@@ -13,7 +13,7 @@ export const resourceUriResolver = {
     if (!path) return path;
     if (process.env.REACT_APP_NATIVE_WRAPPER !== "tauri") return path;
     // Image file in covers/ data directory
-    if (/^\/covers\/[^<>:;,?"*|/]+.(?:jpg|png)$/.test(path)) {
+    if (/^\/covers\/[^<>:;,?"*|/]+\.(?:jpg|png)$/.test(path)) {
       const { tauri } = window.__TAURI__;
       const devicePath = [coversDir, path.substring(8)].join(pathSep);
       return tauri.convertFileSrc(devicePath);

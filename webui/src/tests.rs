5201da4993a947240800f6a20590225d6c57224a
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2022-12-05 08:06:15 +0300

    chore(tests): fix wrong http port

--- webui/src/tests.rs
+++ webui/src/tests.rs
@@ -52,7 +52,7 @@ async fn start_webui() {
     thread::sleep(Duration::from_secs(2));
 
     let client: Client = Config::new()
-        .set_base_url(Url::parse("http://localhost:5051").unwrap())
+        .set_base_url(Url::parse("http://localhost:5053").unwrap())
         .try_into()
         .unwrap();
 
@@ -67,7 +67,7 @@ async fn start_webui() {
       "query": "\n    query IntrospectionQuery {\n      __schema {\n        \n        queryType { name }\n        mutationType { name }\n        subscriptionType { name }\n        types {\n          ...FullType\n        }\n        directives {\n          name\n          description\n          \n          locations\n          args {\n            ...InputValue\n          }\n        }\n      }\n    }\n\n    fragment FullType on __Type {\n      kind\n      name\n      description\n      \n      fields(includeDeprecated: true) {\n        name\n        description\n        args {\n          ...InputValue\n        }\n        type {\n          ...TypeRef\n        }\n        isDeprecated\n        deprecationReason\n      }\n      inputFields {\n        ...InputValue\n      }\n      interfaces {\n        ...TypeRef\n      }\n      enumValues(includeDeprecated: true) {\n        name\n        description\n        isDeprecated\n        deprecationReason\n      }\n      possibleTypes {\n        ...TypeRef\n      }\n    }\n\n    fragment InputValue on __InputValue {\n      name\n      description\n      type { ...TypeRef }\n      defaultValue\n      \n      \n    }\n\n    fragment TypeRef on __Type {\n      kind\n      name\n      ofType {\n        kind\n        name\n        ofType {\n          kind\n          name\n          ofType {\n            kind\n            name\n            ofType {\n              kind\n              name\n              ofType {\n                kind\n                name\n                ofType {\n                  kind\n                  name\n                  ofType {\n                    kind\n                    name\n                  }\n                }\n              }\n            }\n          }\n        }\n      }\n    }\n  "
     }"#;
 
-    let res = surf::post("http://localhost:5051/graphql")
+    let res = surf::post("http://localhost:5053/graphql")
         .header("Accept", "application/json")
         .body(payload)
         .await

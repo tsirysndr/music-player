e46bb1e625a7fdf40063cd84e6ed00653ba2b5a8
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-11-01 07:39:13 +0000

    ci: fix tests

--- .fluentci/src/dagger/jobs.ts
+++ .fluentci/src/dagger/jobs.ts
@@ -14,6 +14,7 @@ export const test = async (src = ".") => {
       .pipeline(Job.test)
       .container()
       .from("rust:1.73-bookworm")
+      .withExec(["apt-get", "update"])
       .withExec([
         "apt-get",
         "install",

d8a9cdd15bd030b23d89a40d6a325bd561ee3613
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-08-25 18:43:19 +0200

    [graphql/tests] add missing global user_data

--- graphql/src/tests/mod.rs
+++ graphql/src/tests/mod.rs
@@ -1,7 +1,9 @@
 use std::{env, sync::Arc};
 
 use async_graphql::Schema;
+use extism::UserData;
 use music_player_addons::{CurrentDevice, CurrentReceiverDevice, CurrentSourceDevice};
+use music_player_host_fn::state::State;
 use music_player_playback::{
     audio_backend::{self, rodio::RodioSink, Sink},
     config::AudioFormat,
@@ -9,7 +11,6 @@ use music_player_playback::{
 };
 use music_player_storage::{searcher::Searcher, Database};
 use music_player_tracklist::Tracklist;
-use music_player_types::types::Device;
 use tokio::sync::{
     mpsc::{UnboundedReceiver, UnboundedSender},
     Mutex,
@@ -55,6 +56,15 @@ pub async fn setup_schema() -> (
     );
 
     let db = Database::new().await;
+
+    let user_data = UserData::new(State {
+        player_cmd_tx: Arc::clone(&cmd_tx),
+        tracklist: Arc::clone(&tracklist),
+        db: db.clone(),
+        addons: vec![],
+        addon_capabilities: vec![],
+    });
+
     (
         Schema::build(
             Query::default(),
@@ -69,6 +79,7 @@ pub async fn setup_schema() -> (
         .data(Arc::clone(&source_device))
         .data(Arc::clone(&receiver_device))
         .data(Arc::clone(&searcher))
+        .data(user_data)
         .finish(),
         Arc::clone(&cmd_tx),
         Arc::clone(&cmd_rx),

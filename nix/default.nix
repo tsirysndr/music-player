b1948154bc2842d57736c76c06ed89ebe7db2fb1
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-09-14 23:59:31 +0300

    add nix/package.nix

--- nix/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
-
-rec {
-  music-player = pkgs.callPackage ./music-player.nix { };
-  music-player-desktop = pkgs.callPackage ./music-player-desktop.nix { };
-  default = music-player;
-}
